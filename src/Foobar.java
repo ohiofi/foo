51073b727f2d10e63a11854d6a0c1e435fd69cfb
ohiofi <20hz20khz@gmail.com>
2018-09-07 00:32:27 -0400

    Initial commit

---
+++ src/Foobar.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Foobar
+{
+	public static void main(String[] args)
+	{
+		int myInt;
+		double myDouble;
+		boolean myBoolean;
+		char myChar;
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Gimme an int: ");
+		myInt = input.nextInt();
+		System.out.print("Gimme a double: ");
+		myDouble = input.nextDouble();
+		System.out.print("Gimme a boolean: ");
+		myBoolean = input.nextBoolean();
+		System.out.print("Gimme a char: ");
+		myChar = input.next().charAt(0);
+		
+		System.out.println();
+		System.out.printf("%20s","Your int was...");
+		System.out.printf("%10d",myInt);
+		System.out.println();
+		System.out.printf("%20s","Your double was...");
+		System.out.printf("%10.2f",myDouble);
+		System.out.println();
+		System.out.printf("%20s","Double that is...");
+		System.out.printf("%10.2f",myDouble*2);
+		System.out.println();
+		System.out.printf("%20s","Your boolean was...");
+		System.out.printf("%10b",myBoolean);
+		myBoolean = !myBoolean;
+		System.out.println();
+		System.out.printf("%20s","The opposite is...");
+		System.out.printf("%10b",myBoolean);
+		System.out.println();
+		System.out.printf("%20s","Your char was...");
+		System.out.printf("%10c",myChar);
+		System.out.println();
+		System.out.printf("%20s","The next char is...");
+		myChar++;
+		System.out.printf("%10c",myChar);
+		
+	}
+}
