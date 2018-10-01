04672b24bb3a7e80259b83d7c5adef9df752d01f
ohiofi <20hz20khz@gmail.com>
2018-10-01 00:59:48 -0400

    testing program expected

--- src/Blah.java
+++ src/Blah.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
+import java.awt.Rectangle;
 
 public class Blah {
 	public static void main(String[] args) {
-		
+		Rectangle box = new Rectangle(5, 10, 20, 30);
+		// Move the rectangle 
+		box.translate(15, 25); 
+		// Print information about the moved rectangle
+		System.out.print("x: "); 
+		System.out.println(box.getX()); 
+		System.out.println("Expected: 15");
+		System.out.print("y: "); 
+		System.out.println(box.getY()); 
+		System.out.println("Expected: 25");
 	}
 }
